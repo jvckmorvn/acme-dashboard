1bd3b5e1661c86ad1ae945b0a4eac5e170148ec9
Jack Moran <jvckmorvn@gmail.com>
2023-11-25 19:32:12 +0200

    remove console.logs

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -15,7 +15,6 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
